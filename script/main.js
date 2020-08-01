34bd02f264d8fb18ea99bb50d814bbad50e753c4
Ionut Rogojan <34418533+ionutrogojan@users.noreply.github.com>
2020-08-02 00:33:51 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,3 +1,6 @@
+window.onblur = function() { document.title = 'come back!'; }
+    window.onfocus = function() { document.title = 'IRP V4'; }
+
 function openClose() {
   document.getElementById('menuNav').classList.toggle('hidden');
   document.getElementById('menuNav').classList.toggle('show');
