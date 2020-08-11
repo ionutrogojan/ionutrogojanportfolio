a84743480867ed78b6d5f40d756c3beb84dc3f0c
Ionut Rogojan <34418533+ionutrogojan@users.noreply.github.com>
2020-08-12 02:53:40 +0300

    cleanup

--- script/main.js
+++ script/main.js
@@ -1,21 +1,23 @@
 window.onblur = function() { document.title = 'come back!'; }
-window.onfocus = function() { document.title = 'IRP V4'; }
+window.onfocus = function() { document.title = 'IRP Cleanup'; }
 
-function openClose() {
-  document.getElementById('menuNav').classList.toggle('hidden');
-  document.getElementById('menuNav').classList.toggle('show');
+function oC() {
+  document.getElementById('mNV').classList.toggle('h');
+  document.getElementById('mNV').classList.toggle('s');
 }
 
-function scrollToLatest() {
-  document.getElementById('latest').scrollIntoView({
+function stt() {
+  document.getElementById('mH').scrollIntoView({
     behavior: 'smooth'
   });
 }
 
-function toTheTop() {
-  document.getElementById('intro').scrollIntoView({
-    behavior: 'smooth'
-  });
+function bti() {
+  window.location.href='../index.html';
+}
+
+function olp() {
+  window.location.href='pages/projects.html';
 }
 
 let i = 0;
@@ -23,13 +25,13 @@ let txt = "> WELCOME"
 let speed = 200;
 
 function typeW() {
-  document.getElementById('type').innerHTML += txt.charAt(i);
+  document.getElementById('t').innerHTML += txt.charAt(i);
   i++;
   setTimeout(typeW, speed);
   //console.log(i);
   if (i === 60) {
     i = 0;
-    document.getElementById('type').innerHTML = '';
+    document.getElementById('t').innerHTML = '';
   }
 }
 typeW();
